8b7051ae509aff01d1205ef8b517a48fb9850830
Adam <adamsion74@gmail.com>
2025-10-24 11:58:12 +0300

    s3

---
+++ gisma-ai-back/gisma-agent-server/src/main/java/iaf/ofek/gisma/ai/service/ingestion/S3Service.java
@@ -0,0 +1,96 @@
+package iaf.ofek.gisma.ai.service.ingestion;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import org.apache.commons.io.FilenameUtils;
+import java.time.Instant;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    public String uploadFile(MultipartFile file) {
+        if (file == null || file.isEmpty()) {
+            return "";
+        }
+
+        try {
+            String key = generateKey(file);
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .contentType(file.getContentType())
+                    .build();
+
+            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
+
+            return getFileUrl(key);
+        } catch (IOException | S3Exception e) {
+            throw new RuntimeException("Failed to upload file to S3", e);
+        }
+    }
+
+    public List<String> uploadFiles(List<MultipartFile> files) {
+        List<String> fileUrls = files != null ? files.parallelStream()
+                .map(this::uploadFile)
+                .toList() : List.of();
+
+        return fileUrls;
+    }
+
+    public void deleteFiles(List<String> files) {
+        if (files != null) {
+            files.parallelStream()
+                    .forEach(this::deleteFile);
+        }
+    }
+
+    public void deleteFile(String fileUrl) {
+        try {
+            String key = (new URI(fileUrl)).getPath().substring(1);
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+            s3Client.deleteObject(deleteObjectRequest);
+        } catch (Exception e) {
+        }
+    }
+
+    private String getFileUrl(String key) {
+        GetUrlRequest request = GetUrlRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+        URL url = s3Client.utilities().getUrl(request);
+
+        return url.toString();
+    }
+
+    private String generateKey(MultipartFile file) {
+        String originalName = file.getOriginalFilename();
+        String baseName = FilenameUtils.getBaseName(originalName);
+        String extension = FilenameUtils.getExtension(originalName);
+
+        return String.format("%s_%s.%s", baseName, Instant.now().toString(), extension);
+    }
+
+}
