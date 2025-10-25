40de5d259df77fe4c3f78dc852ed4fed04a8b29b
Adam <adamsion74@gmail.com>
2025-10-25 10:45:09 +0300

    types

--- gisma-ai-client/types/index.ts
+++ gisma-ai-client/types/index.ts
@@ -2,6 +2,7 @@ export * from './chat';
 export * from './data';
 export * from './error';
 export * from './folder';
+export * from './ingestion';
 export * from './openai';
 export * from './plugin';
 export * from './user';
