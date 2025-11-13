be683f2fce9863aefb37714d8b3e0a0374f31620
Adam <adamsion74@gmail.com>
2025-11-13 18:19:23 +0200

    new design mvp

--- gisma-ai-back/gisma-agent-server/src/main/java/iaf/ofek/gisma/ai/agent/orchestrator/AgentOrchestrator.java
+++ gisma-ai-back/gisma-agent-server/src/main/java/iaf/ofek/gisma/ai/agent/orchestrator/AgentOrchestrator.java
@@ -19,54 +19,16 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class AgentOrchestrator {
 
-    private final RagService ragService;
-
-    private final PreflightClassifierService preflightClassifierService;
-
-    private final ActionModeExecutorRouter actionModeExecutorRouter;
-
     private final OneShotExecutor oneShotExecutor;
-
-//    public Flux<String> handleQuery(UserPromptDTO prompt, UUID userId) {
-//        return ragService.quickShotSimilaritySearch(prompt.query(), userId)
-//                .flatMap(quickShotResponse -> preflightClassifierService.classify(prompt.query(), quickShotResponse, userId))
-//                .flatMapMany(preflightClassifierResponse -> {
-//                    if (preflightClassifierResponse.sufficient()) {
-//                        return Flux.just(preflightClassifierResponse.rephrasedResponse());
-//                    }
-//
-//                    ActionModeExecutor executor = actionModeExecutorRouter.route(preflightClassifierResponse);
-//
-//                    return executor.execute(prompt, preflightClassifierResponse, userId);
-//                });
-//    }
 
     public Flux<String> handleQuery(UserPromptDTO prompt, UUID userId) {
         return oneShotExecutor.execute(prompt, userId);
     }
 
     public String handleQueryBlocking(UserPromptDTO prompt, UUID userId) {
-        QuickShotResponse quickShotResponse = ragService.quickShotSimilaritySearch(prompt.query(), userId).block();
-
-        if (quickShotResponse == null) {
-            throw new IllegalArgumentException("quickShotResponse must not be null");
-        }
-
-        PreflightClassifierResult preflightClassifierResult =  preflightClassifierService.classify(prompt.query(), quickShotResponse, userId).block();
-
-        if (preflightClassifierResult == null) {
-            throw new IllegalArgumentException("quickShotResponse must not be null");
-        }
-
-        if (preflightClassifierResult.sufficient()) {
-            return preflightClassifierResult.rephrasedResponse();
-        }
-
-        ActionModeExecutor executor = actionModeExecutorRouter.route(preflightClassifierResult);
-
         return StringUtils.joinLines(
-                Objects.requireNonNull(executor.execute(prompt, preflightClassifierResult, userId).collectList()                       // Collect all Flux<String> items into List<String>
-                .block())
+                Objects.requireNonNull(oneShotExecutor.execute(prompt, userId).collectList()                       // Collect all Flux<String> items into List<String>
+                        .block())
         );
     }
 
