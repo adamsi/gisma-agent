fd214e2a42cfd600a6b179418234fc9e2bcb8fc0
adam-sion <adamsion74@gmail.com>
2025-11-23 20:55:55 +0200

    remove legacy code

--- gisma-ai-back/gisma-mcp-server/src/main/java/iaf/ofek/gisma/ai/service/tools/GismaServicesTools.java
+++ gisma-ai-back/gisma-mcp-server/src/main/java/iaf/ofek/gisma/ai/service/tools/GismaServicesTools.java
@@ -22,8 +22,8 @@ public class GismaServicesTools {
     }
 
     @Tool(description = "Get fruit by name")
-    public List<JsonNode> getFruitByName(String name) {
-        return gismaServicesClient.getAllFruits();
+    public JsonNode getFruitByName(String name) {
+        return gismaServicesClient.getFruitByName(name);
     }
 
 }
